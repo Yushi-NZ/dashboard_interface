7da32e420fc990e993d824dd56d6dcb864792b1f
Yushi <56866582+Yushi-NZ@users.noreply.github.com>
2026-02-02 21:11:28 +1300

    Delete src/helper/supabaseClient.js

--- src/helper/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://fbvxknntlrywdkurkdja.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZidnhrbm50bHJ5d2RrdXJrZGphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDYxMTksImV4cCI6MjA4NTM4MjExOX0.Qr18KKU04s8IBr_OFBw-rVxKmAqs34Kn_Wwui7s5978";
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default supabase;
