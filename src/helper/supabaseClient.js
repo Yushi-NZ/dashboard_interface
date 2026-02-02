131a9058b55b16f343db7f571267480ac14bccd4
Jordan <yushioce@gmail.com>
2026-02-02 21:21:10 +1300

    modified:   src/helper/supabaseClient.js

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -1,11 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase env vars: VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
+const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ANON_KEY);
+export default supabase;
