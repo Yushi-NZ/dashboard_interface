5d224d2b87a75fe89df6b9b90b29aa35822a2c7d
Jordan <yushioce@gmail.com>
2026-02-02 21:29:49 +1300

    modified:   src/helper/supabaseClient.js

--- src/helper/supabaseClient.js
+++ src/helper/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
