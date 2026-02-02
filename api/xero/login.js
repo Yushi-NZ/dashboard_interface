5553d2de8d726593371d581acd10684cb333bb8f
Jordan <yushioce@gmail.com>
2026-02-02 21:02:46 +1300

    modified:   api/xero/login.js

--- api/xero/login.js
+++ api/xero/login.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const clientId = process.env.XERO_CLIENT_ID;
-  const redirectUri = process.env.XERO_REDIRECT_URI;
+  const redirectUrl = process.env.XERO_REDIRECT_URL;
   const scopes = process.env.XERO_SCOPES;
 
   const state = "xero_" + Date.now(); // you can make this stronger later
@@ -8,10 +8,10 @@ export default async function handler(req, res) {
   const url =
     "https://login.xero.com/identity/connect/authorize" +
     `?response_type=code` +
-    `&client_id=${encodeURIComponent(clientId)}` +
-    `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-    `&scope=${encodeURIComponent(scopes)}` +
-    `&state=${encodeURIComponent(state)}`;
+    `&client_id=${encodeURlComponent(clientId)}` +
+    `&redirect_url=${encodeURLComponent(redirectUrl)}` +
+    `&scope=${encodeURLComponent(scopes)}` +
+    `&state=${encodeURLComponent(state)}`;
 
   res.redirect(url);
 }
