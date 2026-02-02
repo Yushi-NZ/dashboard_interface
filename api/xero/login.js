fa0f0d352c817d9ce5ec075fba14cbbd7fe6c9c4
Jordan <yushioce@gmail.com>
2026-02-02 21:53:39 +1300

    modified:   api/xero/login.js

--- api/xero/login.js
+++ api/xero/login.js
@@ -3,8 +3,6 @@ export default async function handler(req, res) {
   const redirectUrl = import.meta.env.VITE_XERO_REDIRECT_URL;
   const scopes = import.meta.env.VITE_XERO_SCOPES;
 
-  console.log(clientId);
-
   const state = "xero_" + Date.now(); // you can make this stronger later
 
   const url =
@@ -15,5 +13,5 @@ export default async function handler(req, res) {
     `&scope=${encodeURLComponent(scopes)}` +
     `&state=${encodeURLComponent(state)}`;
 
-  res.redirect(url);
+  console.log(url);
 }
