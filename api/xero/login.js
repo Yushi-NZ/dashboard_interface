deeff9ee93542cbb450126a9b274ed0a6c3f0080
Jordan <yushioce@gmail.com>
2026-02-04 19:13:31 +1300

    test

--- api/xero/login.js
+++ api/xero/login.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
-  const clientId = import.meta.env.VITE_XERO_CLIENT_ID;
-  const redirectUrl = import.meta.env.VITE_XERO_REDIRECT_URL;
-  const scopes = import.meta.env.VITE_XERO_SCOPES;
+  const clientId = import.meta.env.XERO_CLIENT_ID;
+  const redirectUrl = import.meta.env.XERO_REDIRECT_URL;
+  const scopes = import.meta.env.XERO_SCOPES;
 
   const state = "xero_" + Date.now(); // you can make this stronger later
 
